b32c8d7a860d5db8fb0b41d26a6dd7d6f1b73e9e
LDAP\dmnhut <dmnhut@cmcglobal.vn>
2022-05-06 17:19:54 +0700

    update functions

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,32 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { Schema, Types } from 'mongoose';
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
   Handler,
 } from 'aws-lambda'
-
+import * as qs from 'query-string'
 const { stringify } = JSON
 
 mongoose.connect('mongodb+srv://mn198:mn198@cluster0.s5xbw.mongodb.net/test_serverless?retryWrites=true&w=majority')
 
+function transformValue(doc, ret: { [key: string]: any }) {
+  delete ret._id;
+}
+
 const Post = new Schema({
   title: String,
   body: String,
+}, {
+  toObject: {
+    virtuals: true,
+    versionKey: false,
+    transform: transformValue,
+  },
+  toJSON: {
+    virtuals: true,
+    versionKey: false,
+    transform: transformValue,
+  },
 });
 
 const PostModel = mongoose.model('posts', Post);
@@ -19,8 +34,8 @@ const PostModel = mongoose.model('posts', Post);
 export const createPost: Handler<
   APIGatewayProxyEvent,
   APIGatewayProxyResult
-> = async function serverlessWebpack(event, context, callback) {
-  const data = JSON.parse(event.body || '');
+> = async (event, context, callback) => {
+  const data = qs.parse(event.body || '');
   const newPost = await PostModel.create(data);
   return {
     body: stringify(newPost),
@@ -28,13 +43,90 @@ export const createPost: Handler<
   }
 }
 
-export const hello: Handler<
+export const getPost: Handler<
   APIGatewayProxyEvent,
   APIGatewayProxyResult
-> = async function serverlessWebpack(event, context, callback) {
-  const data = { hello: 'test' }
+> = async (event, context, callback) => {
+  const id = event.pathParameters?.id || '';
+
+  if (Types.ObjectId.isValid(id)) {
+    const post = await PostModel.findOne({ _id: id });
+    if (post) {
+      return {
+        body: stringify(post),
+        statusCode: 200,
+      }
+    }
+  }
   return {
-    body: stringify(data),
-    statusCode: 200,
+    body: 'id not found',
+    statusCode: 404,
+  }
+}
+
+export const getAllPost: Handler<
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult
+> = async (event, context, callback) => {
+  const post = await PostModel.find({});
+  if (post) {
+    return {
+      body: stringify(post),
+      statusCode: 200,
+    }
+  }
+
+  return {
+    body: 'id not found',
+    statusCode: 404,
+  }
+}
+
+export const updatePost: Handler<
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult
+> = async (event, context, callback) => {
+  const id = event.pathParameters?.id || '';
+  const { title, body } = qs.parse(event.body || '');
+
+  if (Types.ObjectId.isValid(id)) {
+    const post = await PostModel.findOneAndUpdate(
+      { _id: id },
+      { $set: { title, body } },
+      { new: true },
+    );
+    if (post) {
+      return {
+        body: stringify(post),
+        statusCode: 200,
+      }
+    }
+  }
+
+  return {
+    body: 'id not found',
+    statusCode: 404,
+  }
+}
+
+export const deletePost: Handler<
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult
+> = async (event, context, callback) => {
+  const id = event.pathParameters?.id || '';
+
+  if (Types.ObjectId.isValid(id)) {
+    const post = await PostModel.findOneAndRemove({ _id: id });
+    if (post) {
+      return {
+        body: stringify(post),
+        statusCode: 200,
+      }
+    }
+  }
+
+  return {
+    body: 'id not found',
+    statusCode: 404,
   }
 }
